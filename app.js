3017f8f5364b420ad018780ef811dac98209a917
frenz <77576774+Frenzarectah@users.noreply.github.com>
2025-01-07 14:16:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const app = Vue.createApp({
     data() {
         return {
             name:"CalendA",
-            days: [  {
+            days: [  {                //this are the basic variables and objects for the project
                 'num':null,
                 'class': 'empty',
                 'events': []
@@ -29,12 +29,13 @@ const app = Vue.createApp({
             startDay: null,
             currentTime: null,
             token:"",
-            sala_grande: [],
+            sala_grande: [],            //this 2 objects are used to store the "sala_grande (big room)" and "sala piccola (small room)" meetings
             sala_piccola: [],
-            isMeetingRunning: false,
-            current_event: []
+            isMeetingRunning: false,    //a flag used to sign if a meeting is currently running in the specific "now" moment
+            current_event: []           //current event is an object to store the information about the running meeting (above descripted)
         };
     },
+    //this is the template dinamically created according to the days of the current month, some details changes if an event is running
     template:`
                <div id="container" xmlns="http://www.w3.org/1999/html">
                  <header :class="['head', isMeetingRunning ? 'running' : 'not_running']">
@@ -66,22 +67,22 @@ const app = Vue.createApp({
         this.daysInMonth = new Date(this.year, this.month, 0).getDate();
         this.startDay = (this.firstDayOfMonth.getDay() + 5) % 7;
         this.updateTime();
-        setInterval(this.updateTime, 1000);
-        this.get_Meetings();
+        setInterval(this.updateTime, 1000);        //this for updating the clock every minute
+        this.get_Meetings();        //used to check the microsoft API for new meetings
         setInterval(function() {
-            window.location.reload();
+            window.location.reload();    //the webapp needs to re-render the entire template to list the brand new meetings
         }, 300000);
     },
     watch:{
-        'days': {
+        'days': {                        //using a watch to check if there's a running event every time a days is rendered
             handler(newVal) {
                 const now = new Date();
-                const formattedDate = now.toString().substring(0, 21); // Ottieni solo la data nel formato YYYY-MM-DD
+                const formattedDate = now.toString().substring(0, 21); // Force the today data format to be formatted -> YYYY-MM-DD
                 this.isMeetingRunning = newVal.some(day => {
                         if (day.events) {
                             const running_event = day.events.find(event => event.class==='grande' && formattedDate >= event.dateStart && formattedDate <= event.dateEnd)
-                            if (running_event) {
-                                this.current_event = running_event;
+                            if (running_event) {       
+                                this.current_event = running_event; //populate the current_event with the event which has same times as now (formattedDate)
                                 return true;
                             }
                         }
@@ -92,22 +93,22 @@ const app = Vue.createApp({
         }
     },
     methods: {
-        isWeekend(day){
+        isWeekend(day){            //check if the day to render is sunday or saturday 
             let dayofWeek = (new Date(this.year,this.month-1, day).getDay());
             return dayofWeek === 0 || dayofWeek === 6;
         },
         generateDays() {
             this.days = [];
 
-            for (let i = 1; i < this.startDay; i++) { //caselle vuote
+            for (let i = 1; i < this.startDay; i++) {     //empty cells
                 this.days.push({ num: '', class: 'empty' });
             }
-            for (let day = 1; day <= this.daysInMonth; day++) {  //caselle in cui c'è eventi rossi/blu o oggi
-                let _day = day.toString().padStart(2, '0');
+            for (let day = 1; day <= this.daysInMonth; day++) { //check if in the day there are some meetings, and add a class to render them in
+                let _day = day.toString().padStart(2, '0');     //the corrispondent cell
                 let dayClass = '';
                 let events = [];
 
-                const grande = this.sala_grande.filter(event => event.day === _day);
+                const grande = this.sala_grande.filter(event => event.day === _day);  
                 const piccola = this.sala_piccola.filter(event => event.day === _day);
 
                 grande.forEach(grande => {
@@ -143,16 +144,16 @@ const app = Vue.createApp({
             const minutes = String(now.getMinutes()).padStart(2, '0');
             this.currentTime = `${hours}:${minutes}`;
         },
-        async lime_login(){
-            let data = JSON.stringify({
-                "email": "staging@grupposinergia.com",
-                "password": "Pa$$woRd?123"
+        async lime_login(){                        //these are some endpoint for my proxy API which communicates with microsoft graph
+            let data = JSON.stringify({            //for security issue, it will be not listed/explained in this repo.
+                "email": *removed*,
+                "password": *removed*
             });
 
             let config = {
                 method: 'post',
                 maxBodyLength: Infinity,
-                url: 'https://lime.grupposinergia.net/api/login',
+                url: 'https://*removed*/api/login',
                 headers: {
                     'Content-Type': 'application/json'
                 },
@@ -177,7 +178,7 @@ const app = Vue.createApp({
             let config = {
                 method: 'get',
                 maxBodyLength: Infinity,
-                url: 'https://lime.grupposinergia.net/api/v2/all_meetings',
+                url: 'https://*removed*/api/v2/all_meetings',
                 headers: {
                     'Content-Type': 'application/json',
                     'company-code': 'XX',
